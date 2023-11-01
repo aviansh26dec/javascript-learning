f8b4b3452e24d5749f0960c7c95be554e4ffe8b3
Avinash Kumar Singh <avinashkumarsingh.ignou@gmail.com>
2023-11-01 10:23:32 +0000

    let, const and var

---
+++ 01-variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountEmail = "avinash@gmail.com";
+var accountPassword = "123456";
+accountCity = "Luckow";
+let accountState;
+
+// accountId = 2; //not allowed
+accountEmail = "ravi@gmail.com"; //edit allowed
+accountPassword = "2626";
+accountCity = "Noida";
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
